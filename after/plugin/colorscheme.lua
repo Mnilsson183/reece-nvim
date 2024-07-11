86a9408ffcb00e969c5e30b7cd8f67060d3b8f06
Reece Hutchison <hutchisora@icloud.com>
2024-07-11 17:19:07 -0600

    nvim setup

---
+++ after/plugin/colorscheme.lua
@@ -0,0 +1,6 @@
+function colormypencil(color)
+	color = color or "solarized"
+	vim.cmd.colorscheme(color)
+end
+
+colormypencil()
---
+++ init.lua
@@ -0,0 +1,4 @@
+require("reece")
+require("reece.remap")
+require("reece.set")
+require("reece.packer")
